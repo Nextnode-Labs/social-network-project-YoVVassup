aec5a7365124a4c1312ca98bb8cf2d9b154afaa1
yowassup <yowassup1992@gmail.com>
2022-01-28 14:34:27 +0300

    Updates

---
+++ precache-manifest.c2dfc2e751ebfa48a49a4a82f4c27329.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5553be13dcf0f605aca61592619cf799",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7647016e430ab28cdcd0",
+    "url": "/static/css/2.bbddbf26.chunk.css"
+  },
+  {
+    "revision": "dd5c4b093692a5a415b6",
+    "url": "/static/css/3.8b453797.chunk.css"
+  },
+  {
+    "revision": "e1e7b1ea0af364cf1a08",
+    "url": "/static/css/4.9afbba54.chunk.css"
+  },
+  {
+    "revision": "5d004c2c233ac67677f5",
+    "url": "/static/css/main.333da93d.chunk.css"
+  },
+  {
+    "revision": "7647016e430ab28cdcd0",
+    "url": "/static/js/2.043115f8.chunk.js"
+  },
+  {
+    "revision": "b78d06d995050d23ec508cb4ba84482c",
+    "url": "/static/js/2.043115f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd5c4b093692a5a415b6",
+    "url": "/static/js/3.0fccccb8.chunk.js"
+  },
+  {
+    "revision": "e1e7b1ea0af364cf1a08",
+    "url": "/static/js/4.f1d51a44.chunk.js"
+  },
+  {
+    "revision": "32996b5c970a2bc82b5a",
+    "url": "/static/js/5.035c36b5.chunk.js"
+  },
+  {
+    "revision": "5d004c2c233ac67677f5",
+    "url": "/static/js/main.dbbdbf5d.chunk.js"
+  },
+  {
+    "revision": "f273df4548b07dbba05d",
+    "url": "/static/js/runtime-main.c053a895.js"
+  },
+  {
+    "revision": "702af69bcf6f20040c805e8cc19b9911",
+    "url": "/static/media/preloader.702af69b.svg"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "/static/media/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "/static/media/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "/static/media/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "/static/media/primeicons.e01fd413.eot"
+  },
+  {
+    "revision": "37a74fdb2324f19cbbbd2b62937a168f",
+    "url": "/static/media/user.37a74fdb.png"
+  }
+]);
